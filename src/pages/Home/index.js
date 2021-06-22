c9563c5815d3c0fe21f59032e65a098736097d86
lucas torres <lucaskauoke@hotmail.com>
2021-06-22 15:44:36 -0300

    Tela cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,60 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-import {Text} from 'react-native';
-
-
-
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
 
 import {
-  Container,
+  styles,
 } from './styles';
 
-export default class Home extends Component {
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
 
-  render() {
+const App = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "black" : "white";
+    const color = item.id === selectedId ? 'white' : 'black';
+
     return (
-      <Container>
-        <Text>Lista</Text>
-      </Container>
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
     );
-  }
-}
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
